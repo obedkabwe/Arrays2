d85ace916c06f98cfc64ebfaac13372c9fe568c7
obed <91228819+obedkabwe@users.noreply.github.com>
2022-12-09 13:22:04 -0600

    Add files via upload

---
+++ Exercises3.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exercises3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Scanner sc= new Scanner(System.in);
+		//int sum = 0;
+		int [] vect = new int[] {10,20,-8,17,-23,25,7,-2};
+		
+		for (int i = 0; i < vect.length; i++) {
+		//	System.out.println(vect[i]);
+		
+		}
+		//show the negative integers
+		// if the vect < 0
+		for (int i = 0; i < vect.length; i++) {
+			if (vect[i] < 0) {
+				System.out.println(vect[i]);
+			}
+		}
+		
+		
+
+	}
+
+}
